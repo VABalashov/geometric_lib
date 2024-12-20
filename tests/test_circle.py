8db64ce8c8e10bedbe0cfd7efb15a1806f1ed48f
Vladimir Balashov <143887060+VABalashov@users.noreply.github.com>
2024-12-21 02:08:53 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,10 +1,10 @@
-import circle
 import sys
 import os
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
+import circle
 
 
 def test_area():
