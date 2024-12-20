f407e9af7abe92a6a9640e8df57f5ae0df7f19e8
Vladimir Balashov <143887060+VABalashov@users.noreply.github.com>
2024-12-21 02:09:22 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,10 +1,10 @@
-import triangle
 import sys
 import os
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
+import triangle
 
 
 def test_area():
