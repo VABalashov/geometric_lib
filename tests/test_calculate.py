39d5132c0a4278eaf25a6e71bf635ea155700dda
Vladimir Balashov <143887060+VABalashov@users.noreply.github.com>
2024-12-21 02:08:42 +0300

    Update test_calculate.py

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,10 +1,10 @@
-import calculate
 import sys
 import os
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
+import calculate
 
 
 def test_valid():
