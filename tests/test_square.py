db8789a3058f1147fad4c9b2e1dc52a7e51f2a26
Vladimir Balashov <143887060+VABalashov@users.noreply.github.com>
2024-12-21 02:09:10 +0300

    Update test_square.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,10 +1,10 @@
-import square
 import sys
 import os
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
+import square
 
 
 def test_area():
